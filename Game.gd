33d2f275e389e7f24301968fa1051839d84cfc99
Grégoire Juge <gregoire.juge@gmail.com>
2021-01-26 10:52:26 +0100

    Add 3i and 9i anoms

--- Game.gd
+++ Game.gd
@@ -1,10 +1,11 @@
 extends Node
 
 var currentAnom: int = 0
+var currentAnomLabel: String
 var oldAnom: int = 0
 var score: int = 0
 var failed: bool = false
-var array = [1,2,3,4,5,6,7,8,9,10,11,12]
+var array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 var sampled
 
 # Called when the node enters the scene tree for the first time.
@@ -28,19 +29,27 @@ func _on_SpawnTimer_timeout():
 	for anom in sampled:
 		get_node("MeshInstance/Anomaly"+str(anom)).show()
 	currentAnom = sampled.pop_front()
-	$NextLabel.text = "Next: " + str(currentAnom) + "h"
+	currentAnomLabel = get_currentAnomLabel(currentAnom)
+	$NextLabel.text = "Next: " + currentAnomLabel
 	$DestroyTimer2.start()
 	$DestroyTimer4.start()
 	$DestroyTimer6.start()
 	$DestroyTimer8.start()
 
+func get_currentAnomLabel(currentAnom):
+	if currentAnom == 13:
+		currentAnomLabel = "3i"
+	elif currentAnom == 14:
+		currentAnomLabel = "9i"
+	else:
+		currentAnomLabel = str(currentAnom) + "h"
 
 func _on_DestroyTimer_timeout():
-	#get_node("MeshInstance/Anomaly"+str(currentAnom)).hide()
 	check_failed()
 	if len(sampled) > 0:
 		currentAnom = sampled.pop_front()
-		$NextLabel.text = "Next: " + str(currentAnom) + "h"
+		currentAnomLabel = get_currentAnomLabel(currentAnom)
+		$NextLabel.text = "Next: " + currentAnomLabel
 	else:
 		$NextLabel.text = "Please wait..."
 		$SpawnTimer.start()
@@ -48,14 +57,11 @@ func _on_DestroyTimer_timeout():
 func check_failed():
 	$FailedLabel.hide()
 	get_node("MeshInstance/Anomaly"+str(currentAnom)).hide()
-
 	var anom = get_node("MeshInstance/Anomaly"+str(currentAnom))
 	var player = $StaticBody
 	var player_transform_2d = Vector2(player.transform.basis.x.x, player.transform.basis.x.z).normalized()
 	var direction = anom.transform.origin - player.transform.origin
 	var direction_2d = Vector2(direction.x, direction.z)
-	#var a = Vector2(direction.normalized().x, direction.normalized().z)
-	#var b = Vector2(player.transform.basis.x.x, player.transform.basis.x.z)
 	var a = Vector2(direction_2d.normalized().x, direction_2d.normalized().y)
 	var b = Vector2(player_transform_2d.x, player_transform_2d.y)
 	var delta = rad2deg(acos(a.dot(b)))
